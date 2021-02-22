ab48610a082b30e7e65d00c08e52bbdc520a882a
howard <Howard@howards-mac.lan>
2021-02-22 16:38:37 -0500

    rewrote schema to create tables as the keys weren't correct

--- table_create.sql
+++ table_create.sql
@@ -1,4 +1,26 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_mananger;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
 
+-- create employees table
+CREATE TABLE public.employees
+(
+    emp_no serial NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex text NOT NULL,
+	hire_date date NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+ALTER TABLE public.employees
+    OWNER to postgres;
+	
 -- ALTER TABLE public.employees
 --     ADD CONSTRAINT employees_fkey FOREIGN KEY (emp_no)
 --     REFERENCES public.titles (title_id)
@@ -11,8 +33,9 @@
 CREATE TABLE public.salaries
 (
     emp_no serial NOT NULL,
-    salary int,
-    PRIMARY KEY (emp_no)
+    emp_title VARCHAR NOT NULL,
+    PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
 );
 
 ALTER TABLE public.salaries
@@ -21,9 +44,10 @@ ALTER TABLE public.salaries
 -- create titles table
 CREATE TABLE public.titles
 (
-    title_id serial NOT NULL,
-    title text,
-    PRIMARY KEY (title_id)
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY (title_id),
+	FOREIGN KEY (title_id) REFERENCE employees.employee_title_id	
 );
 
 ALTER TABLE public.titles
@@ -34,7 +58,8 @@ CREATE TABLE public.dept_emp
 (
     emp_no serial NOT NULL,
     dept_no text NOT NULL,
-    PRIMARY KEY (dept_no)
+    PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
 );
 
 ALTER TABLE public.dept_emp
@@ -45,7 +70,8 @@ CREATE TABLE public.dept_manager
 (
     dept_no text NOT NULL,
     emp_no serial NOT NULL,
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
 );
 
 ALTER TABLE public.dept_manager
@@ -56,7 +82,8 @@ CREATE TABLE public.deptartments
 (
     dept_no text NOT NULL,
     dept_name text NOT NULL,
-    PRIMARY KEY (dept_no)
+    PRIMARY KEY (dept_no),
+	FOREIGN KEY (dept_no) REFERENCES dept_emp.dept_no
 );
 
 ALTER TABLE public.deptartments
