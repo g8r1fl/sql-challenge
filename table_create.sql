dcf6147511f4191d7f4778ae26b43d66fd46bcbb
howard <Howard@howards-mac.lan>
2021-02-22 17:21:19 -0500

    had the wrong FK's in tables so couldn't create departments or title tables.

--- table_create.sql
+++ table_create.sql
@@ -1,9 +1,16 @@
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_mananger;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
+-- DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS dept_emp;
+-- DROP TABLE IF EXISTS dept_mananger;
+-- DROP TABLE IF EXISTS departments;
+-- DROP TABLE IF EXISTS salaries;
+-- DROP TABLE IF EXISTS titles;
+
+DROP TABLE employees CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_mananger CASCADE;
+DROP TABLE departments CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
 
 -- create employees table
 CREATE TABLE public.employees
@@ -16,6 +23,7 @@ CREATE TABLE public.employees
 	sex text NOT NULL,
 	hire_date date NOT NULL,
     PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 ALTER TABLE public.employees
@@ -35,7 +43,7 @@ CREATE TABLE public.salaries
     emp_no serial NOT NULL,
     emp_title VARCHAR NOT NULL,
     PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 ALTER TABLE public.salaries
@@ -46,8 +54,8 @@ CREATE TABLE public.titles
 (
     title_id VARCHAR NOT NULL,
     title VARCHAR NOT NULL,
-    PRIMARY KEY (title_id),
-	FOREIGN KEY (title_id) REFERENCE employees.employee_title_id	
+    PRIMARY KEY (title_id)
+-- 	FOREIGN KEY (title_id) REFERENCES employees (emp_title_id)
 );
 
 ALTER TABLE public.titles
@@ -59,7 +67,7 @@ CREATE TABLE public.dept_emp
     emp_no serial NOT NULL,
     dept_no text NOT NULL,
     PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 ALTER TABLE public.dept_emp
@@ -71,7 +79,8 @@ CREATE TABLE public.dept_manager
     dept_no text NOT NULL,
     emp_no serial NOT NULL,
     PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees.emp_no
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 ALTER TABLE public.dept_manager
@@ -82,10 +91,12 @@ CREATE TABLE public.deptartments
 (
     dept_no text NOT NULL,
     dept_name text NOT NULL,
-    PRIMARY KEY (dept_no),
-	FOREIGN KEY (dept_no) REFERENCES dept_emp.dept_no
+    PRIMARY KEY (dept_no)
+-- 	FOREIGN KEY (dept_no) REFERENCES dept_manager(dept_no)
 );
 
 ALTER TABLE public.deptartments
     OWNER to postgres;
+	
+
 	
